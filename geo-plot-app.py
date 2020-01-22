f5f99201d7b816cea80ea3336bc78d43cffdd273
Noah Sanders <noasanders@ucdavis.edu>
2020-01-22 01:18:10 -0800

    Added Dropdown -> load image path functionality | Removed bicycle class initialization

--- geo-plot-app.py
+++ geo-plot-app.py
@@ -9,31 +9,59 @@ import bicycleparameters as bp
 ###!!!Must ensure that current working directory is set to directory immediatly containing '\data\'!!!###
 ###---------------------------------------------------------------------------------------------------###
 
-### Initializes Benchmark and draws the geometry plot ###
-def draw_plot():
-    bike = bp.Bicycle("Benchmark", pathToData=os.getcwd()+"\\data")
-    plot = bike.plot_bicycle_geometry()
-    plot.savefig(os.getcwd()+'\\assets\\plot.png')
-
 app = dash.Dash(__name__)
 
 app.layout = html.Div([
-    html.H1('Benchmark Bicycle Geometry Plot'),
-    html.Button("Calculate Geometry Plot",  
-                id="plot-button",
-                type="button",
-                n_clicks=0),
-    html.Img(src='/assets/plot.png',  
+    html.H1('Bicycle Geometry Plot'),
+    dcc.Dropdown(id="bike-dropdown",
+                 options=[{'label': 'Benchmark', 'value': 'Benchmark'},
+                          {'label': 'Browser', 'value': 'Browser'},
+                          {'label': 'Browserins', 'value': 'Browserins'},
+                          {'label': 'Crescendo', 'value': 'Crescendo'},
+                          {'label': 'Fisher', 'value': 'Fisher'},
+                          {'label': 'Pista', 'value': 'Pista'},
+                          {'label': 'Rigid', 'value': 'Rigid'},
+                          {'label': 'Silver', 'value': 'Silver'},
+                          {'label': 'Tms', 'value': 'Tms'},
+                          {'label': 'Yellow', 'value': 'Yellow'},
+                          {'label': 'Yellowrev', 'value': 'Yellowrev'}],
+                 value='Benchmark') , 
+    html.Img(src='',  
              alt='A plot revealing the general geometry, centers of mass and moments of inertia of the given bicycle system.',
              id="geometry-plot",
              width="640",
-             height="480")
+             height="480"),
 ])
 
-@app.callback(Output('geometry-plot', ''),
-              [Input('plot-button', 'n_clicks')])
-def update_output(n_clicks):
-    return draw_plot()
+    # Updates image path with dropdown value
+@app.callback(Output('geometry-plot', 'src'), [Input('bike-dropdown', 'value')])
+def reveal_plot(value):
+    image = str(value)+'.png'
+    path = '/assets/defaults/'+image
+    return path
 
 if __name__ == '__main__':
     app.run_server(debug=True)
+
+#(For safe keeping)
+''' 
+html.Button("Calculate Geometry Plot",  
+                id="calculate-button",
+                type="button",
+                n_clicks=0),
+'''
+
+'''
+### Initializes Benchmark and draws the geometry plot ###
+def new_plot():
+    bike = bp.Bicycle("Benchmark", pathToData=os.getcwd()+"\\data")
+    plot = bike.plot_bicycle_geometry()
+    plot.savefig(os.getcwd()+'\\assets\\plot.png')
+'''
+
+'''
+    # Change to add to list of plots, but not actually show it
+@app.callback(Output('geometry-plot', 'src'), [Input('calculate-button', 'n_clicks')])
+def calculate_geo_plot(n_clicks):
+    return draw_plot()
+'''
